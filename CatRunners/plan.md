05261bc4b057eab92a51b6343b07fa896f563484
Sanjay Singh <sanjaygurjar741@gmail.com>
2025-02-17 23:18:34 +0530

    update

---
+++ CatRunners/plan.md
@@ -0,0 +1,4 @@
+### Process:
+- Make a window
+- Put a cat in middle
+- Make the cat draggable
--- Learnings/main.cpp
+++ Learnings/main.cpp
@@ -1,20 +1,32 @@
-#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    sf::Window window(sf::VideoMode(800, 600), "My window");
-
-    // run the program as long as the window is open
+    
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    
     while (window.isOpen())
     {
-        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Text text;
+        
+        text.setString("Hello world");
+        text.setCharacterSize(24);
+        text.setFillColor(sf::Color::Red);
+        text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+        window.draw(text);
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // "close requested" event: we close the window
+            
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+        window.clear(sf::Color::White);
+        
+        
+        window.display();
+        
     }
 
     return 0;

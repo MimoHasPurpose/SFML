f5dd4b652976439f73d601e9b735a86c69951d10
Sanjay Singh <sanjaygurjar741@gmail.com>
2025-02-23 05:58:06 +0530

    mouse events

--- mouseAndKey/main.cpp
+++ mouseAndKey/main.cpp
@@ -2,10 +2,10 @@
 #include<iostream>
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800,800), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(400,400), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
-
+    bool mouseWasReleased=false;
     while (window.isOpen())
     {
         sf::Event event;
@@ -13,11 +13,20 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+
+            if(event.type==sf::Event::MouseButtonPressed){
+                std::cout<<"mouse button pressed"<<std::endl;
+            }
+            mouseWasReleased=false;
+            if(event.type==sf::Event::MouseButtonReleased){
+                mouseWasReleased=true;
+            }
         }
 
-        if(sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)){
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)&& mouseWasReleased){
             std::cout<<"Left mouse button pressed"<<std::endl;
         }
+      
 
         window.clear();
         window.draw(shape);

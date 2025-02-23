48051ec07d3f43c80351c8a751d3d5e88b088807
Sanjay Singh <sanjaygurjar741@gmail.com>
2025-02-23 07:13:40 +0530

    setframerate

--- fonts/main.cpp
+++ fonts/main.cpp
@@ -8,8 +8,14 @@ int main()
     font.loadFromFile("./oj.ttf");
     sf::Text text("Hello sexy",font,35);
     text.move(35.0f,0.0f);
+
+    sf::Clock clock;
     while (window.isOpen())
     {
+        //compute frame rate
+        float ct=clock.restart().asSeconds();
+        float fps=1.0f/(ct);
+        std::cout<<"fps: "<<fps<<std::endl;
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -22,6 +28,7 @@ int main()
         // window.draw(shape);
         window.draw(text);
         window.display();
+        window.setFramerateLimit(16);
     }
 
     return 0;
